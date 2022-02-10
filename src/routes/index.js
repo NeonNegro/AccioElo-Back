ba46b3a8515e17b6a98aa603e435923b2d93b2d9
deboracaires <deboracaires@pop-os.localdomain>
2022-02-10 17:55:58 -0300

    created routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,20 @@
 import {Router} from "express";
+import authRouter from "./authRouter.js";
+import checkoutRouter from "./checkoutRouter.js";
 import healthRouter from "./healthRouter.js";
+import productsRouter from "./productsRouter.js";
 
  
 const router = Router();
+
 router.use(healthRouter);
+
+router.use(authRouter);
+
+// not authenticated route
+router.use(productsRouter);
+
+//authenticated route
+router.use(checkoutRouter);
 
 export default router;
